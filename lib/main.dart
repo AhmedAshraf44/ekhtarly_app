54178f721327432e94ab3269baeb22d379744203
AhmedAshraf44 <aa5485111@gmail.com>
2024-04-16 20:46:50 +0200

    editing in andriod dirctory

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:device_preview/device_preview.dart';
 import 'package:ekhtarly_app/core/utils/app_router.dart';
 import 'package:ekhtarly_app/core/utils/service_locator.dart';
 import 'package:ekhtarly_app/features/authentication/data/repos/auth_repo_impl.dart';
@@ -17,9 +16,10 @@ import 'package:google_fonts/google_fonts.dart';
 void main() {
   setupServiceLocator();
   runApp(
-    DevicePreview(
-      enabled: true,
-      builder: (context) => const EkhtarlyApp(),),);
+    // DevicePreview(
+    //   enabled: true,
+    //   builder: (context) => 
+      const EkhtarlyApp(),);
 }
 
 class EkhtarlyApp extends StatelessWidget {
@@ -68,8 +68,8 @@ class EkhtarlyApp extends StatelessWidget {
       ],
       child:
        MaterialApp.router(
-        locale: DevicePreview.locale(context),
-        builder: DevicePreview.appBuilder,
+        // locale: DevicePreview.locale(context),
+        // builder: DevicePreview.appBuilder,
         debugShowCheckedModeBanner: false,
         routerConfig: AppRouter.router,
         theme: ThemeData(textTheme:GoogleFonts.interTextTheme(),
